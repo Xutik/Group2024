ff67542435008772aa224c489e51a5e111b4e488
Kevin Garcia Martin <kevingm@live.se>
2024-11-19 15:43:17 +0100

    Render projects

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import './styles/reset.css';
 import './styles/style.css';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import renderProjects from './js/projects'
+
+renderProjects();
--- src/js/projects.js
+++ src/js/projects.js
@@ -1,3 +1,11 @@
+import calculatorKGM from '../assets/img/calculator-kgm.PNG'
+import calculatorKJW from '../assets/img/calculator-kjw.png'
+import calculatorXutik from '../assets/img/calculator-xutik.jpg'
+import etchASketchKJW from '../assets/img/etch-a-sketch-kjw.png'
+import restaurantKGM from '../assets/img/restaurant-kgm.PNG'
+import tictactoeKlejda from '../assets/img/tictactoe-klej.png'
+import tictactoeXutik from '../assets/img/tictactoe-xutik.jpg'
+
 const project = (title, techStack, author, image, link) => {
   return {
     title,
@@ -13,54 +21,95 @@ const projects = [
     'Calculator',
     ['JavaScript', 'CSS', 'HTML'],
     'Kevin Garcia Martin',
-    'src/assets/img/calculator-kgm.PNG',
+    calculatorKGM,
     'https://kevingarciamartin.github.io/calculator/'
   ),
   project(
-    'Fjordsmaken',
+    'Restaurant',
     ['Webpack', 'JavaScript', 'CSS', 'HTML'],
     'Kevin Garcia Martin',
-    'src/assets/img/restaurant-kgm.PNG',
+    restaurantKGM,
     'https://kevingarciamartin.github.io/restaurant-page/'
   ),
   project(
     'Etch-A-Sketch',
     ['JavaScript', 'CSS', 'HTML'],
     'Kiattisak Jod Wongphayak',
-    'src/assets/img/etch-a-sketch-kjw.png',
+    etchASketchKJW,
     'https://maybejod.github.io/ODIN-etch-a-sketch/'
   ),
   project(
     'Calculator',
     ['JavaScript', 'CSS', 'HTML'],
     'Kiattisak Jod Wongphayak',
-    'src/assets/img/calculator-kjw.png',
+    calculatorKJW,
     'https://maybejod.github.io/ODIN-calculator/'
   ),
   project(
     'Tic-Tac-Toe',
     ['JavaScript', 'CSS', 'HTML'],
     'Klejda Mamaj',
-    'src/assets/img/tictactoe-klej.png',
-    '#'
+    tictactoeKlejda,
+    'https://github.com/klejda59/tictactoe.git'
+  ),
+  project(
+    'Restaurant',
+    ['Webpack', 'JavaScript', 'CSS', 'HTML'],
+    'Klejda Mamaj',
+    '',
+    'https://github.com/klejda59/restaurant-page.git'
   ),
   project(
     'Calculator',
     ['JavaScript', 'CSS', 'HTML'],
     'Kseniia Prakhova',
-    'src/assets/img/calculator-xutik.jpg',
-    '#'
+    calculatorXutik,
+    'https://xutik.github.io/calculator/'
   ),
   project(
     'Tic-Tac-Toe',
     ['JavaScript', 'CSS', 'HTML'],
     'Kseniia Prakhova',
-    'src/assets/img/tictactoe-xutik.jpg',
-    '#'
+    tictactoeXutik,
+    'https://xutik.github.io/TicTacToe/'
   )
 ]
 
 export default function renderProjects() {
+  const projectsSection = document.querySelector('#projects')
+  const projectsSectionTitle = document.createElement('h2')
+  const gridContainer = document.createElement('div')
+
+  gridContainer.classList.add('grid-container')
+
+  projectsSectionTitle.textcontent = 'Projects'
+
+  projectsSection.appendChild(projectsSectionTitle)
+  projectsSection.appendChild(gridContainer)
+
+  projects.forEach((project) => {
+    const card = document.createElement('article')
+
+    card.classList.add('card')
+
+    card.innerHTML = `
+      <img class="card-img" src="${project.image}" alt="project image">
+      <div class="card-text">
+        <div class="card-title">
+          <h3>${project.title}</h3>
+          <div class="card-link">
+            <a href="${project.link}" target="_blank">
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" />
+            </a>
+          </div>
+        </div>
+        <p class="card-author">${project.author}</p>
+        <p class="card-tech-stack">${project.techStack.join(', ')}</p>
+      </div>
+    `
+
+    gridContainer.appendChild(card)
+  })
   // <h2>Projects</h2>
   //       <div class="grid-container">
   //           <div class="card">
