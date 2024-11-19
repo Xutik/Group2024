5231bc0ef67e7da20eaeb9e5bbbcc98aeadb9be8
Kevin Garcia Martin <kevingm@live.se>
2024-11-19 10:09:24 +0100

    Add project class

--- src/projects.js
+++ src/projects.js
@@ -1,12 +1,20 @@
-const projects = [
-    {
-      "name": "Recipes",
-      "programming languages": "html",
-      "author": "", 
-      "tags": "", 
-      "links": "", 
-    },
-    {
+const project = (title, techStack, author, image, link) => {
+  return {
+    title,
+    techStack,
+    author,
+    image,
+    link,
+  }
+}
 
-    }
+export const projects = [
+  project(
+    'Calculator',
+    ['JavaScript', 'CSS', 'HTML'],
+    'Kevin Garcia Martin',
+    'image link',
+    'https://kevingarciamartin.github.io/calculator/',
+  ),
+  project('title2', ['lang3', 'lang4', 'lang5'])
 ]
