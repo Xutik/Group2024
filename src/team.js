b51fb78c2ba23835659992247e6a6d47defeaca9
Kevin Garcia Martin <kevingm@live.se>
2024-11-19 10:08:14 +0100

    Add teamMember class and fill out team

--- src/team.js
+++ src/team.js
@@ -1,10 +1,14 @@
-const team = [
-    {
-      "name": "Jod",
-      "contacts": "@",
-      "links": "https://github.com/MaybeJod", 
-    },
-    {
+const teamMember = (name, contacts, links) => {
+  return {
+    name,
+    contacts,
+    links,
+  }
+}
 
-    }
+export const team = [
+    teamMember('Kevin Garcia Martin', 'kevin.garcia@hyperisland.se', 'https://github.com/kevingarciamartin'),
+    teamMember('Kiattisak Jod Wongphayak', 'kiattisak.wongphayak@hyperisland.se', 'https://github.com/MaybeJod'),
+    teamMember('Klejda Mamaj', 'klejda.mamaj@hyperisland.se', 'https://github.com/klejda59'),
+    teamMember('Kseniia Prakhova', 'kseniia.prakhova@hyperisland.se', 'https://github.com/Xutik')
 ]
