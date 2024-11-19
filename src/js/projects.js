ffd13f8b2874987efb32cbddc3666ff215a8380f
Kevin Garcia Martin <kevingm@live.se>
2024-11-19 11:31:56 +0100

    Modularize html

--- src/js/projects.js
+++ src/js/projects.js
@@ -8,7 +8,7 @@ const project = (title, techStack, author, image, link) => {
   }
 }
 
-export const projects = [
+const projects = [
   project(
     'Calculator',
     ['JavaScript', 'CSS', 'HTML'],
@@ -38,3 +38,141 @@ export const projects = [
     'https://maybejod.github.io/ODIN-calculator/'
   )
 ]
+
+export default function renderProjects() {
+  // <h2>Projects</h2>
+  //       <div class="grid-container">
+  //           <div class="card">
+  //               <div class="card-img"></div>
+  //               <div class="card-text">
+  //                   <div class="card-title">
+  //                       <h3>Project name</h3>
+  //                       <div class="card-link">
+  //                           <a href="https://github.com/MaybeJod" target="_blank">
+  //                               <img
+  //                                   src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" />
+  //                           </a>
+  //                           <a href="https://codepen.io/notjod" target="_blank">
+  //                               <img
+  //                                   src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/codepen/codepen-original.svg" />
+  //                           </a>
+  //                       </div>
+  //                   </div>
+  //                   <p>
+  //                       Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+  //                       Mollitia, laborum!
+  //                   </p>
+  //               </div>
+  //           </div>
+  //           <div class="card">
+  //               <div class="card-img"></div>
+  //               <div class="card-text">
+  //                   <div class="card-title">
+  //                       <h3>Project name</h3>
+  //                       <div class="card-link">
+  //                           <a href="https://github.com/MaybeJod" target="_blank">
+  //                               <img
+  //                                   src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" />
+  //                           </a>
+  //                           <a href="https://codepen.io/notjod" target="_blank">
+  //                               <img
+  //                                   src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/codepen/codepen-original.svg" />
+  //                           </a>
+  //                       </div>
+  //                   </div>
+  //                   <p>
+  //                       Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+  //                       Mollitia, laborum!
+  //                   </p>
+  //               </div>
+  //           </div>
+  //           <div class="card">
+  //               <div class="card-img"></div>
+  //               <div class="card-text">
+  //                   <div class="card-title">
+  //                       <h3>Project name</h3>
+  //                       <div class="card-link">
+  //                           <a href="https://github.com/MaybeJod" target="_blank">
+  //                               <img
+  //                                   src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" />
+  //                           </a>
+  //                           <a href="https://codepen.io/notjod" target="_blank">
+  //                               <img
+  //                                   src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/codepen/codepen-original.svg" />
+  //                           </a>
+  //                       </div>
+  //                   </div>
+  //                   <p>
+  //                       Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+  //                       Mollitia, laborum!
+  //                   </p>
+  //               </div>
+  //           </div>
+  //           <div class="card">
+  //               <div class="card-img"></div>
+  //               <div class="card-text">
+  //                   <div class="card-title">
+  //                       <h3>Project name</h3>
+  //                       <div class="card-link">
+  //                           <a href="https://github.com/MaybeJod" target="_blank">
+  //                               <img
+  //                                   src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" />
+  //                           </a>
+  //                           <a href="https://codepen.io/notjod" target="_blank">
+  //                               <img
+  //                                   src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/codepen/codepen-original.svg" />
+  //                           </a>
+  //                       </div>
+  //                   </div>
+  //                   <p>
+  //                       Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+  //                       Mollitia, laborum!
+  //                   </p>
+  //               </div>
+  //           </div>
+  //           <div class="card">
+  //               <div class="card-img"></div>
+  //               <div class="card-text">
+  //                   <div class="card-title">
+  //                       <h3>Project name</h3>
+  //                       <div class="card-link">
+  //                           <a href="https://github.com/MaybeJod" target="_blank">
+  //                               <img
+  //                                   src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" />
+  //                           </a>
+  //                           <a href="https://codepen.io/notjod" target="_blank">
+  //                               <img
+  //                                   src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/codepen/codepen-original.svg" />
+  //                           </a>
+  //                       </div>
+  //                   </div>
+  //                   <p>
+  //                       Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+  //                       Mollitia, laborum!
+  //                   </p>
+  //               </div>
+  //           </div>
+  //           <div class="card">
+  //               <div class="card-img"></div>
+  //               <div class="card-text">
+  //                   <div class="card-title">
+  //                       <h3>Project name</h3>
+  //                       <div class="card-link">
+  //                           <a href="https://github.com/MaybeJod" target="_blank">
+  //                               <img
+  //                                   src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" />
+  //                           </a>
+  //                           <a href="https://codepen.io/notjod" target="_blank">
+  //                               <img
+  //                                   src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/codepen/codepen-original.svg" />
+  //                           </a>
+  //                       </div>
+  //                   </div>
+  //                   <p>
+  //                       Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+  //                       Mollitia, laborum!
+  //                   </p>
+  //               </div>
+  //           </div>
+  //       </div>
+}
