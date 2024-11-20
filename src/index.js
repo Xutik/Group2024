1bfdb7555448a1be42b87fe2d1e55405bb0adbf5
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2024-11-20 10:55:46 +0100

    feat: added hero section, styled it

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import './styles/reset.css'
 import './styles/style.css'
 
 import createNav from './js/header.js'
+import createHeroSection from './js/hero.js';
 import { createTeamSection } from './js/team.js';
 import renderProjects from './js/projects'
 
 document.addEventListener('DOMContentLoaded', () => {
     createNav()
-    renderProjects();
-  
+    renderProjects()
+    createHeroSection()
+
     const main = document.querySelector('main') || document.body;
     const team = createTeamSection();
     main.appendChild(team);
